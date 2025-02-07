e9903937864bc6a3dd05b093dec748daa1da5e61
Sai Nayunipati <visa423@gmail.com>
2025-02-07 15:02:29 -0500

    Update briefme.bash

--- briefme.bash
+++ briefme.bash
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 formatted_date=$(date +"%B %d, %Y")
+
+echo ""
+
 echo "Today is ${formatted_date}."
 
 echo ""
+
+echo "ECONOMICS"
 
 UPR=$(curl -s https://fred.stlouisfed.org/series/UNRATE/ | \
 grep -m 1 -oP '<span class="series-meta-observation-value">\K[^<]+')
 
 echo "Unemployment Rate: ${UPR}%"
 
-CSI=$(curl -s https://fred.stlouisfed.org/series/CSUSHPINSA | \
+echo ""
+
+echo "REAL ESTATE"
+
+THIRTY_YEAR=$(curl -s https://fred.stlouisfed.org/series/MORTGAGE30US | \
 grep -m 1 -oP '<span class="series-meta-observation-value">\K[^<]+')
 
-echo "Case-Schiller: ${CSI}"
+echo "30-Year Fixed Rate Mortgage: ${THIRTY_YEAR}%"
+
+FIFTEEN_YEAR=$(curl -s https://fred.stlouisfed.org/series/MORTGAGE15US | \
+grep -m 1 -oP '<span class="series-meta-observation-value">\K[^<]+')
+
+echo "15-Year Fixed Rate Mortgage: ${FIFTEEN_YEAR}%"
